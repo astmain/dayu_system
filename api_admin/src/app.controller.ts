06fa23980bd712c215240446f7f3038554789bed
xupeng <1311192345@qq.com>
2025-04-28 18:09:22 +0800

    111

--- api_admin/src/app.controller.ts
+++ api_admin/src/app.controller.ts
@@ -5,7 +5,9 @@ import {ApiBearerAuth, ApiBody, ApiParam} from '@nestjs/swagger';
 import {CreateVehicleDto} from './CreateVehicleDto';
 // import aaa from "../prisma/tb_account"
 let tb_account: any;
-tb_account = require("../prisma/tb_account");
+// tb_user = require("../prisma/tb_user");
+tb_user = require("../prisma/tb_user.js");
+
 // import tb_account from '../prisma/tb_user.js';
 
 @ApiTags('测试接口')
@@ -16,7 +18,7 @@ export class AppController {
     @ApiOperation({summary: ''})
     async local() {
         // console.log(`tb_account---find_all:`,  tb_account)
-        console.log(`tb_account---find_all:`, await tb_account.find_all())
+        console.log(`tb_account---find_all:`, await tb_user.find_all())
         return {code: 200, msg: '成功', result: 111};
     }
 
