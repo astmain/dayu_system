6d7634670cc6b6aef5fab6c87e106fae0dc5471c
xupeng <1311192345@qq.com>
2025-04-30 18:25:24 +0800

    vue_admin

--- render_admin/src/Layout/app.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-
-let app = createApp(App)
-import 'element-plus/dist/index.css'
-import ElementPlus from "element-plus"
-
-app.use(ElementPlus, {size: "small"})
-export default app
