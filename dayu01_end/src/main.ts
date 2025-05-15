85c9e9bc78285bf9fd09fedaf1a7d87c93ecc2eb
xupeng <1311192345@qq.com>
2025-05-15 19:54:49 +0800

    响应拦截器,异常拦截器

--- dayu01_end/src/main.ts
+++ dayu01_end/src/main.ts
@@ -6,17 +6,17 @@ import { join } from 'path'
 // 自定义
 import {AppModule} from './app.module'
 import {config_docs} from "./config_docs"
-
-
-
 import {Config_logger_global_middleware} from "./Config_logger_global_middleware"
-
+import {Config_filter_response} from "./Config_filter_response"
+import {Config_filter_catch_error} from "./Config_filter_catch_error"
 
 async function bootstrap() {
     let app_http = await NestFactory.create<NestExpressApplication>(AppModule, {cors: true})
     app_http.useStaticAssets(join(__dirname,  'static'),{prefix:"/static"})   //      http://127.0.0.1:3000/static/1747306430929.png
     let main = config_docs(app_http, 3000)
     main.app.use(Config_logger_global_middleware)
+    main.app.useGlobalInterceptors(new Config_filter_response())
+    main.app.useGlobalFilters(new Config_filter_catch_error())
     await main.app.listen(main.port);
 
 
