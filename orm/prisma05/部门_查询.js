d921ba6e8f114ed7f30aab0403595182f863ea10
xupeng <1311192345@qq.com>
2025-05-21 14:44:32 +0800

    orm

--- orm/prisma05/部门_查询.js
+++ orm/prisma05/部门_查询.js
@@ -4,6 +4,11 @@ async function make() {
     let {PrismaClient} = require("@prisma/client")
     let prisma = new PrismaClient()
 
+    await prisma.tb_depart.findMany({where:{id:1}})
+
+
+
+
     console.log('数据初始化完成');
 
 
